f4fbcb1d996b43d944a41c62b91ab5204f74130b
mk <max.kieler@grauraum.net>
2018-04-27 12:30:19 +0200

    Enhance Code Organisation by transfering in class structure.

--- ImperfectQuantumCopyMachine.py
+++ ImperfectQuantumCopyMachine.py
@@ -1,5 +1,14 @@
-from qiskit import QuantumProgram
+try:
+    import qiskit
+    from qiskit import QuantumProgram
+except ImportError:
+    print("Module qiskit does not exists")
+    exit()
+if (qiskit.__version__ != "0.4.8"):
+    print("Module version of qiskit is not tested. There could occure Problems.")
+
 import numpy as np
+from functools import reduce
 
 __doc__=    """ The Program performs an imperfect copy of a qubit state
                 $\ket{\Phi}$ using an additional single quantum register such
@@ -23,44 +32,90 @@ __doc__=    """ The Program performs an imperfect copy of a qubit state
                 """
 
 
-qp = QuantumProgram()
-qr = qp.create_quantum_register('qr',3)
-cr = qp.create_classical_register('cr',3)
-qc = qp.create_circuit('ImperfectQuantumCopy',[qr],[cr])
+class QuantumAlgorithm:
+    def __init__(self, name, N_qm, N_cl):
+        self.name = name
+        self.N_qm = N_qm
+        self.N_cl = N_cl
+        self.input_state = [0]*self.N_qm
+        self.quantum_program = QuantumProgram()
+        self.quantum_register = self.quantum_program.create_quantum_register('qr',3)
+        self.classical_register = self.quantum_program.create_classical_register('cr',3)
+        self.quantum_circuit = self.quantum_program.create_circuit(
+                                                    self.name,
+                                                    [self.quantum_register],
+                                                    [self.classical_register])
+        self.result = None
 
-inp = input("val: ")
+    def invert_nth_qbit(self, n):
+        """Invert the qbit at position n. Invert means 0 to 1 and 1 to 0. This
+        is done by using the pauli_x matrix."""
+        assert n >= 0 , "negativ index"
+        assert n < self.N_qm, "index above size of register"
+        self.quantum_circuit.x(self.quantum_register[n])
+        self.input_state[n] = self.input_state[n] ^ 1 #Inversion by XOR
+
+    def get_input_state(self):
+        return self.input_state
 
 
-if inp == "000":
-    pass
-elif inp == "100":
-    qc.x(qr[2])
-elif inp == "H00":
-    qc.h(qr[2])
+    def measure_nth_qbit(self, n):
+        assert n >= 0 , "negativ index"
+        assert n < self.N_qm, "index above size of quantum register"
+        assert n < self.N_cl, "index above size of classical register"
+        self.quantum_circuit.measure(   self.quantum_register[n],
+                                        self.classical_register[n])
+
+    def run(self):
+        self.result = self.quantum_program.execute(self.name)
+
+    def get_result(self):
+        return self.result.get_counts(self.name)
+
+    def add_gate(self, name, register_list, *arg):
+        if name == "pauli_x":
+            self.quantum_circuit.x(self.quantum_register[register_list[0]])
+        elif name == "pauli_y":
+            self.quantum_circuit.y(self.quantum_register[register_list[0]])
+        elif name == "pauli_z":
+            self.quantum_circuit.z(self.quantum_register[register_list[0]])
+        elif name == "hadamard":
+            self.quantum_circuit.h( self.quantum_register[register_list[0]])
+        elif name == "CNOT":
+            self.quantum_circuit.cx(    self.quantum_register[register_list[0]],
+                                        self.quantum_register[register_list[1]])
+        elif name == "rotate_y":
+            self.quantum_circuit.ry(    arg[0],
+                                        self.quantum_register[register_list[0]])
 
 
-a = 2
-b = 1
-c = 0
-
+print(__doc__)
 alpha = np.pi/8.0
 beta = -np.sqrt(np.arcsin(0.5 - np.sqrt(2)/3.0))
 
-qc.ry(alpha, qr[b])
-qc.cx(qr[b], qr[c])
-qc.ry(beta, qr[c])
-qc.cx(qr[c], qr[b])
-qc.ry(alpha, qr[b])
+algo = QuantumAlgorithm('ImperfectQuantumCopy', 3, 3)
+#algo.invert_nth_qbit(0)
 
-qc.cx(qr[a], qr[b])
-qc.cx(qr[a], qr[c])
-qc.cx(qr[b], qr[a])
-qc.cx(qr[c], qr[a])
+algo.add_gate("rotate_y", [1], alpha)
+algo.add_gate("CNOT", [1, 2])
+algo.add_gate("rotate_y", [1], beta)
+algo.add_gate("CNOT", [2, 1])
+algo.add_gate("rotate_y", [1], alpha)
 
-qc.measure(qr[a], cr[0])
-qc.measure(qr[b], cr[1])
-#qc.measure(qr[2], cr[2])
+algo.add_gate("CNOT", [0, 1])
+algo.add_gate("CNOT", [0, 2])
+algo.add_gate("CNOT", [1, 0])
+algo.add_gate("CNOT", [2, 0])
+
+algo.measure_nth_qbit(0)
+algo.measure_nth_qbit(1)
+algo.measure_nth_qbit(2)
+
+algo.run()
+
+input_state = algo.get_input_state()
+
+print("Initial state is: " + reduce(lambda x, y: str(x) + str(y),  input_state))
 
 
-result = qp.execute('ImperfectQuantumCopy')
-print(result.get_counts('ImperfectQuantumCopy'))
+print(algo.get_result())
