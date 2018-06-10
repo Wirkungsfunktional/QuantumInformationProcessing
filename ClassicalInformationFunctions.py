2da8e0ced4acacbfd0e4aaf0b35fcee557c24e51
mk <max.kieler@grauraum.net>
2018-06-10 22:02:17 +0200

    add typing annotations

--- ClassicalInformationFunctions.py
+++ ClassicalInformationFunctions.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.stats as sst
 import MatrixFunctions as MF
-
+from typing import Dict, Tuple, List, Set
 
 __doc__ = """Collection of functions used in classical information theory. The
 implementation is not made in respect to efficiency. There are a lot of tools
@@ -9,11 +9,11 @@ already existence in scipy or numpy. This modul is only for learning purpous,
 how to write such a code by myself. The learning outcome is a better
 understanding of the Theory and not a fast and efficent library."""
 
-def classical_entropy(p):
+def classical_entropy(p: np.ndarray) -> double:
     assert check_l1_norm(p), "p is not l1-normed"
     return sst.entropy(p, base=2)
 
-def conditional_entropy(p_x, p_yx):
+def conditional_entropy(p_x: np.ndarray, p_yx: np.ndarray) -> double:
     assert MF.check_matrix_simple_stochastic(p_yx), "conditional matrix p_yx is not \
     simple stochastic."
     assert check_l1_norm(p_x), "p_x is not l1-normed"
@@ -23,18 +23,18 @@ def conditional_entropy(p_x, p_yx):
     return H
 
 
-def check_l1_norm(p):
+def check_l1_norm(p: np.ndarray) -> bool:
     return np.isclose(np.sum(p), 1)
 
-def check_joint_probability_matrix(m):
+def check_joint_probability_matrix(m: np.ndarray) -> bool:
     return np.isclose(np.sum(m), 1.0)
 
-def joint_entropy(p_xy):
+def joint_entropy(p_x: np.ndarrayy) -> double:
     assert check_joint_probability_matrix(p_xy), "p_xy is not a joint probability \
     matrix."
     return classical_entropy(p_xy.flatten())
 
-def get_conditional_prob_from_joint_prob(p_xy):
+def get_conditional_prob_from_joint_prob(p_xy: np.ndarray) -> np.ndarray:
     assert check_joint_probability_matrix(p_xy), "p_xy is not a joint probability \
     matrix."
     size_a, size_b = p_xy.shape
@@ -46,7 +46,7 @@ def get_conditional_prob_from_joint_prob(p_xy):
     is not a conditional probability matrix"
     return p_xy
 
-def mutual_information(p_x, p_yx):
+def mutual_information(p_x: np.ndarray, p_yx: np.ndarray) -> double:
     assert MF.check_matrix_simple_stochastic(p_yx), "conditional matrix p_yx is not \
     simple stochastic."
     assert check_l1_norm(p_x), "p_x is not l1-normed"
@@ -57,26 +57,26 @@ def mutual_information(p_x, p_yx):
 def information_flow(f, H):
     return f * H
 
-def kullback_leibler_distance(p, q):
+def kullback_leibler_distance(p: np.ndarray, q: np.ndarray) -> double:
     assert check_l1_norm(p), "p is not l1-normed"
     assert check_l1_norm(q), "q is not l1-normed"
     return sst.entropy(p, q, base=2)
 
-def get_alphabet(code_dict):
+def get_alphabet(code_dict: Dict[str, str]) -> Set[str]:
     alphabet = set()
     for word, code in code_dict.items():
         for letter in code:
             alphabet.add(letter)
     return alphabet
 
-def check_kraft_inequality(code_dict):
+def check_kraft_inequality(code_dict: Dict[str, str]) -> bool:
     D = len(get_alphabet(code_dict))
     s = 0
     for word, code in code_dict.items():
         s += 1/D**len(code)
     return (s <= 1)
 
-def check_fano_condition(code_dict):
+def check_fano_condition(code_dict: Dict[str, str]) -> bool:
     code_words = [code for w, code in code_dict.items()]
     code_words = sorted(code_words, key=len)
     while len(code_words) != 0:
@@ -86,7 +86,7 @@ def check_fano_condition(code_dict):
                 return False
     return True
 
-def analyse_code(code_dict):
+def analyse_code(code_dict: Dict[str, str]):
     alphabet = get_alphabet(code_dict)
     flag_kraft_inequality = check_kraft_inequality(code_dict)
     flag_fano = check_fano_condition(code_dict)
--- MatrixFunctions.py
+++ MatrixFunctions.py
@@ -1,58 +1,59 @@
 import numpy as np
 import scipy.linalg as sl
+from typing import Dict, Tuple, List
 
 
 """Check Tools -----------------------------------------------------------"""
 
-def check_matrix_square(m):
+def check_matrix_square(m: np.ndarray) -> bool:
     a, b = m.shape
     return (a == b)
 
-def check_matrix_symmetric(m):
+def check_matrix_symmetric(m: np.ndarray) -> bool:
     assert check_matrix_square(m), "matrix is not a square matrix"
     return np.allclose(m, np.transpose(m))
 
-def check_matrix_hermitian(m):
+def check_matrix_hermitian(m: np.ndarray) -> bool:
     assert check_matrix_square(m), "matrix is not a square matrix"
     return np.allclose(m, np.transpose(np.conjugate(m)))
 
-def check_matrix_positiv(m):
+def check_matrix_positiv(m: np.ndarray) -> bool:
     pass
 
-def check_matrix_unitary(m):
+def check_matrix_unitary(m: np.ndarray) -> bool:
     a, b = m.shape
     return (np.allclose(np.dot(m, np.conjugate(m.T)), np.eye(a)) and \
             np.allclose(np.dot(np.conjugate(m.T), m), np.eye(b)))
 
-def check_matrix_normal(m):
+def check_matrix_normal(m: np.ndarray) -> bool:
     pass
 
-def check_matrix_orthogonal(m):
+def check_matrix_orthogonal(m: np.ndarray) -> bool:
     a, b = m.shape
     return (np.allclose(np.dot(m, m.T), np.eye(a)) and \
             np.allclose(np.dot(m.T, m), np.eye(b)))
 
-def check_matrix_complex(m):
+def check_matrix_complex(m: np.ndarray) -> bool:
     pass
 
-def check_matrix_diagonal(m):
+def check_matrix_diagonal(m: np.ndarray) -> bool:
     pass
 
-def check_matrix_triagonal(m):
+def check_matrix_triagonal(m: np.ndarray) -> bool:
     z = np.zeros_like(m)
     return (np.allclose(np.triu(m, 1), z) or np.allclose(np.tril(m, -1), z))
 
-def check_matrix_singular(m):
+def check_matrix_singular(m: np.ndarray) -> bool:
     pass
 
-def check_matrix_simple_stochastic(m):
+def check_matrix_simple_stochastic(m: np.ndarray) -> bool:
     """Check that the sum over each row is 1."""
     size_a, size_b = m.shape
     one_row = np.ones(size_b)
     res_vec = np.dot(m, one_row)
     return np.allclose(res_vec, np.ones(size_a))
 
-def check_matrix_double_stochastic(m):
+def check_matrix_double_stochastic(m: np.ndarray) -> bool:
     """Check that the sum over each row is 1 and the sum over each column is 1."""
     return (check_matrix_simple_stochastic(m) and
             check_matrix_simple_stochastic(m.T))
@@ -60,7 +61,11 @@ def check_matrix_double_stochastic(m):
 
 """Analyse Tools -----------------------------------------------------------"""
 
-def analyse_matrix_sparse(m, rtol_places=8):
+def analyse_matrix_sparse(m: np.ndarray, rtol_places=8) -> Tuple[int, int]:
+    """Count the number n of non zero entries of a input matrix m and the total
+    number of entries N and return them as a tuple:
+    @input: np.ndarray
+    @return: (number of non zero entries, number of total entries)"""
     a, b = m.shape
     N = a*b
     m = np.round(m, rtol_places)
@@ -72,12 +77,12 @@ def analyse_matrix_sparse(m, rtol_places=8):
 """Creation Tools -----------------------------------------------------------"""
 
 
-def make_matrix_random_unitary(n, m, mu_real=1, mu_imag=1):
+def make_matrix_random_unitary(n: int, m: int, mu_real=1, mu_imag=1) -> np.ndarray:
     H = mu_real*np.random.randn(n, m) + mu_imag*1.j*np.random.randn(n, m)
     Q, R = sl.qr(H)
     return Q
 
-def make_matrix_random_triagonal(n, m, mu_real=1, mu_imag=1):
+def make_matrix_random_triagonal(n: int, m: int, mu_real=1, mu_imag=1) -> np.ndarray:
     H = mu_real*np.random.randn(n, m) + mu_imag*1.j*np.random.randn(n, m)
     Q, R = sl.qr(H)
     return R
